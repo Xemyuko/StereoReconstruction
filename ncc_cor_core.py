b625ee6de6a277e66dd00edfc9869607e2818bb0
Myugeli <myueyg@gmail.com>
2023-04-16 11:26:12 +0200

    Create ncc_cor_core.py
    Backend

---
+++ ncc_cor_core.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 16 11:23:50 2023
+
+@author: myuey
+"""
+import numpy as np
+import scripts as scr
+import cv2 as cv
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+import numba
+from numba import jit, cuda, njit
+def startup_load():
+    pass
+def cor_acc_linear():
+    pass
