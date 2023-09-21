a59033ab016f1f9d7aa19875f90e539f0f11ae35
Xemyuko <myueyg@gmail.com>
2023-09-21 08:32:31 +0200

    testing tmod finding

--- testing_scripts.py
+++ testing_scripts.py
@@ -57,7 +57,7 @@ def t1():
     print(Q)
     print('A________')
     print(geom_arr[ind])
-def distance(pt1,pt2):
+def distance3D(pt1,pt2):
     res = np.sqrt((pt1[0]-pt2[0])**2 + (pt1[1]-pt2[1])**2 + (pt1[2]-pt2[2])**2)
     return res
 def t2():
@@ -77,45 +77,96 @@ def t2():
     config.mat_folder = folder_statue + matrix_folder
     config.tmod = 1.0
     config.speed_mode = 1
-    config.left_folder = left_folder
-    config.right_folder = right_folder
-    config.precise = 1
-    config.corr_map_out = 0
-    #use internal reconstruction
-    ptsL,ptsR = ncc.run_cor_internal(config)
-    rng = np.random.default_rng()
-    indA1 = rng.integers(len(ptsL),size =20)
-    indA2 = rng.integers(len(ptsL),size =20)
-    ptAX1 = []
-    
-    ptAX2 = []
-    for i,j in zip(indA1,indA2):
-        ptAX1.append()
-    
-    kL, kR, r_vec, t_vec = scr.initial_load(1.0,folder_statue +matrix_folder)
-    kL_inv = np.linalg.inv(kL)
-    kR_inv = np.linalg.inv(kR)
-    tri_res = scr.triangulate_list(ptsL,ptsR, r_vec, t_vec, kL_inv, kR_inv, config.precise)
-def t3():
-    #set reference pcf file, folders for images and matrices. 
-    folder_statue = "./test_data/statue/"
-    matrix_folder = "matrix_folder/"
-    left_folder = "camera_L/"
-    right_folder = "camera_R/"
-    input_data = "Rekonstruktion30.pcf"
-    #known correct tmod
-    t_mod = 0.416657633
-    #load reference data
-    xy1,xy2,geom_arr,col_arr,correl = scr.read_pcf(folder_statue + input_data)
-    #create config object
-    config = ch.ConfigHandler()
-    #assign config values
-    config.mat_folder = folder_statue + matrix_folder
-    config.tmod = 1.0
-    config.speed_mode = 1
-    config.left_folder = left_folder
-    config.right_folder = right_folder
+    config.left_folder = folder_statue + left_folder
+    config.right_folder = folder_statue + right_folder
     config.precise = 1
     config.corr_map_out = 0
     #use internal correlation
+    ptsL,ptsR = ncc.cor_internal(config)
+    #rng comparison point indices
+    comp_num = 20
+    rng = np.random.default_rng()
+    indA1 = rng.integers(len(ptsL),size =comp_num)
+    indA2 = rng.integers(len(ptsL),size =comp_num)
+    #pull points out from ptsL and ptsR
+    ptA1 = []
+    ptA2 = []
+    for i in indA1:
+        ptA1.append([ptsL[i], ptsR[i]])
+    for i in indA2:
+        ptA2.append([ptsL[i],ptsR[i]])
+    #find matching points to get comparison points
+    #loop through first point pairs
+    matched_inds1 = []
+    for test_points in ptA1:
+        '''
+        inLx = i[0][0]
+        inLy = i[0][1]
+        inRx = i[1][0]
+        inRy = i[1][1]
+        '''
+        diff1 = 100000.0
+        match1Ind = 0
+        #loop through paired reference points
+        for ind in range(len(xy1)):
+            refPointL = xy1[ind]
+            refPointR = xy2[ind]
+            #compare first set
+            difL = test_points[0] - refPointL
+            difR = test_points[1] - refPointR
+            #sum of squared differences
+            diff_comp = np.sum(difL - difR)**2
+            if diff_comp < diff1:
+                diff1 = diff_comp
+                match1Ind = ind
+        matched_inds1.append(match1Ind)
+    matched_inds2 = []
+    #loop through second point pairs
+    for i in ptA2:
+        diff2 = 100000.0
+        match2Ind = 0
+        for ind in range(len(xy1)):
+            refPointL = xy1[ind]
+            refPointR = xy2[ind]
+            
+            #compare first set
+            difL = test_points[0] - refPointL
+            difR = test_points[1] - refPointR
+            #sum of squared differences
+            diff_comp = np.sum(difL - difR)**2
+            if diff_comp < diff2:
+                diff2 = diff_comp
+                match2Ind = ind
+        matched_inds2.append(match2Ind)
     
+    #retrieve found matching reference points
+    ptB1 = []
+    
+    ptB2 = []
+    for i in matched_inds1:
+        ptB1.append([xy1[i], xy2[i], geom_arr[i]])
+    for i in matched_inds2:
+        ptB2.append([xy1[i], xy2[i], geom_arr[i]])
+    #check first comparison values
+    print(ptB1[0])
+    print("______________________________________")
+    print(ptA1[0])
+    #calculate distances between reference points
+    ref_dist = []
+    for i in range(len(ptB1)):
+        dist = distance3D(ptB1[2],ptB2[2])
+        ref_dist.append(dist)
+    
+    #create loop for testing incremental values of t-vector scaling factor
+    kL, kR, r_vec, t_vec = scr.initial_load(1.0,folder_statue +matrix_folder)
+    kL_inv = np.linalg.inv(kL)
+    kR_inv = np.linalg.inv(kR)
+    inc = 0.001
+    max_tmod = 1
+    tmod_start = inc
+    #create new set of point pairs
+    while tmod_start < max_tmod:
+        t_vec *= tmod_start
+        tri_res = scr.triangulate_list(ptsL,ptsR, r_vec, t_vec, kL_inv, kR_inv, config.precise)
+        
+t2()
