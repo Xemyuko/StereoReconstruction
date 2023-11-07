bb0bc32f90d7b7e1cda9b129815035205ff869b1
Myugeli <myueyg@gmail.com>
2023-11-07 13:15:10 +0100

    cleanup

--- scale_testing.py
+++ scale_testing.py
@@ -16,9 +16,13 @@ def accel_outliers():
     pass
 def remove_outliers(data, thresh_dist):
     p1 = find_centroid(data)
-    for i in tqdm(range(data.shape[0])):
+    res_arr = []
+    for i in range(data.shape[0]):
         p2 = data[i,:]
         dist = np.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 + (p1[2]-p2[2])**2)
+        if dist < thresh_dist:
+            res_arr.append(p2)
+    return np.asarray(res_arr)
 def sort_depths(data):
     pass
 @numba.jit(nopython=True)
@@ -34,7 +38,7 @@ def accel_dist_count(data, data_point, data_ind, thresh_dist, thresh_count):
                 if counter > thresh_count:
                     break
     return counter
-def remove_noise(data,thresh_dist, thresh_count = 50):
+def remove_noise(data,thresh_dist, thresh_count = 60):
     #Calculate the distance from a chosen point to all other points. Stop if counter of points
     #within threshold distance reaches check threshold. If stopped, add point to new data array. 
     #Possibly run this on numba?
@@ -45,6 +49,11 @@ def remove_noise(data,thresh_dist, thresh_count = 50):
         if counter > thresh_count:
             res.append(data[i,:])
     return np.asarray(res)
+
+def cleanup(data,thresh_dist_scale, thresh_count, outlier_scale):
+    #Generate 
+    pass
+    
 def runA1(): 
     data_filepath = './test_data/calibObjects/000POS0Rekonstruktion30.pcf'
     xy1,xy2,geom_arr,col_arr,correl = scr.read_pcf(data_filepath)
@@ -64,11 +73,14 @@ def runA1():
     print('SCALE CHECK: ' + str(dist_scale))
     print('')
     thresh_dist = dist_scale*2
-    res_arr = remove_noise(geom_arr, thresh_dist)
+    res_arr1 = remove_outliers(geom_arr, thresh_dist*80)
     print(' ')
-    print(res_arr.shape)
+    print(res_arr1.shape)
+    res_arr2 = remove_noise(res_arr1, thresh_dist)
     scr.convert_np_ply(geom_arr,col_arr,'calib0.ply', overwrite = True)
-    col_arr1 = scr.gen_color_arr_black(res_arr.shape[0])
-    scr.convert_np_ply(res_arr,col_arr1,'calib1.ply', overwrite = True)
-    print(find_centroid(geom_arr))
+    col_arr1 = scr.gen_color_arr_black(res_arr1.shape[0])
+    scr.convert_np_ply(res_arr1,col_arr1,'calib1.ply', overwrite = True)
+    col_arr2 = scr.gen_color_arr_black(res_arr2.shape[0])
+    scr.convert_np_ply(res_arr2,col_arr2,'calib2.ply', overwrite = True)
+    print(res_arr2.shape)
 runA1()
