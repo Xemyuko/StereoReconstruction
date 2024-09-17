175b9cd6b27bfc36ac27da5db7202abc5fc76161
Myugeli <myueyg@gmail.com>
2024-09-17 13:46:51 +0200

    subwindow creation successful

--- main_window.py
+++ main_window.py
@@ -10,20 +10,21 @@ import recon_window as rw
 
 
 
-
+version = 0.1
 
 root = tk.Tk()
-root.title("Stereo Reconstruction Main Window")
+root.title("Stereo Reconstruction FSU Jena - v" + str(version))
 def open_recon_window():
     rw.start_recon(root)
 def open_cap_window():
     pass
-
+def open_cal_winodw():
+    pass
 rec_btn = tk.Button(root, text = "Reconstruction", command  = open_recon_window)
 rec_btn.pack()
 cap_btn = tk.Button(root, text = "Image Capture", command = open_cap_window)
 cap_btn.pack()
-root.geometry('100x100')
+root.geometry('400x200')
 root.resizable(width=False, height=False)
 root.focus_force()
 root.mainloop()
