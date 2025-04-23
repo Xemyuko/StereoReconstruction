7989ff33cf808958f4de38ec1e14be96aca2ef51
Myugeli <myueyg@gmail.com>
2025-04-23 15:22:12 +0200

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -8,6 +8,7 @@
 #include <cstdlib>
 #include <vector>
 #include <sstream>
+#include <opencv2/calib3d.hpp>
 using namespace cv;
 using namespace std;
 
@@ -42,7 +43,7 @@ cv::Mat_<double> read_matrix(String filepath, int rows, int cols, int skiprows)
     String delim2 = " ";
     cv::Mat_<double> P(rows, cols);
     std::vector<std::string> v = split(file_contents, delim);
-    
+
     for (int i = skiprows; i < v.size(); i++)
     {
         std::vector<std::string> vr = split(v[i], delim2);
@@ -50,7 +51,7 @@ cv::Mat_<double> read_matrix(String filepath, int rows, int cols, int skiprows)
         for (int j = 0; j < vr.size(); j++)
         {
             P.at<double>(i - skiprows, j) = stod(vr[j]);
-        }   
+        }
     }
     return P;
 }
@@ -70,6 +71,8 @@ void write_matrix(String filepath, Mat_<double> P, bool isHeader) {
     }
     wmat.close();
 }
+
+
 
 void load_lr_images(String img_folder, String ext, bool isGray, vector<Mat>& imagesL, vector<Mat>& imagesR) {
     String target = img_folder + "*" + ext;
@@ -97,10 +100,60 @@ void load_lr_images(String img_folder, String ext, bool isGray, vector<Mat>& ima
     }
 }
 
-void rectify_pair(Mat_<double> kL, Mat_<double> kR, Mat_<double> R, Mat_<double> t, Mat imgL, Mat imgR, Mat& imgL_rect, Mat& imgR_rect) {
+void find_pts_SIFT(Mat img1, Mat img2) {
 
 }
 
+void rectify_pair_SIFT() {
+
+}
+
+
+
+void rectify_pair(Mat imgL, Mat imgR, Mat_<double> kL, Mat_<double> kR, Mat_<double> distL, Mat_<double> distR, Mat_<double> R, Mat_<double> t, Mat& imgL_rect, Mat& imgR_rect) {
+    Mat R1, R2, P1, P2, Q;
+    cv::stereoRectify(kL, distL, kR, distR, imgL.size(), R, t, R1, R2, P1, P2, Q);
+    Mat rmap[2][2];
+    cv::initUndistortRectifyMap(kL, distL, R1, P1, imgL.size(), CV_16SC2, rmap[0][0], rmap[0][1]);
+    cv::initUndistortRectifyMap(kR, distR, R2, P2, imgL.size(), CV_16SC2, rmap[1][0], rmap[1][1]);
+    cv::remap(imgL, imgL_rect, rmap[0][0], rmap[0][1], INTER_LINEAR, BORDER_DEFAULT, Scalar());
+    cv::remap(imgR, imgR_rect, rmap[1][0], rmap[1][1], INTER_LINEAR, BORDER_DEFAULT, Scalar());
+}
+
+void rectify_pair_lz() {
+
+}
+
+Mat half_dim(Mat im) {
+    Mat res;
+    cv::resize(im, res, cv::Size(im.cols / 2, im.rows / 2));
+    return res;
+}
+
+Mat create_stereo(Mat im0, Mat im1, bool isSave = false) {
+    Mat res;
+    hconcat(im0, im1, res);
+    res = half_dim(res);
+    if (isSave) {
+        imwrite("stereo.jpg", res);
+    }
+    return res;
+}
+
+
+Mat create_4_view(Mat im0, Mat im1, Mat im2, Mat im3, bool isSave = false) {
+    Mat res1;
+    Mat res2;
+    hconcat(im0, im1, res1);
+    hconcat(im2, im3, res2);
+    Mat res3;
+    vconcat(res1, res2, res3);
+    res3 = half_dim(res3);
+    if (isSave) {
+        imwrite("comp4view.jpg", res3);
+    }
+    return res3;
+}
 
 
 
@@ -109,36 +162,31 @@ int main()
     String data_folder = "C:/Users/Admin/Documents/GitHub/StereoReconstruction/test_data/testset1/";
     String img_folder = data_folder + "bulb-multi/b1/";
 
-    std::vector<String> fn;
-    cv::glob(img_folder + "*.jpg", fn, false);
     vector<Mat> imagesL;
     vector<Mat> imagesR;
     bool isGray = false;
     load_lr_images(img_folder, ".jpg", isGray, imagesL, imagesR);
     cv::Mat img1 = imagesL[0];
     cv::Mat img2 = imagesR[0];
-    cv::Mat img3;
-    hconcat(img2, img2, img3);
-    int new_width = 1456;
-    int new_height = 544;
-    Mat img3_disp;
-    cv::resize(img3, img3_disp, cv::Size(new_width, new_height));
-    
-    cv::imshow("pr", img3_disp);
+
+    //cv::Mat img3 = create_stereo(img1,img2);
+    //cv::imshow("pr", img3);
+    //cv::waitKey(0);
+
+
+    String mat_fol = data_folder + "matrices/";
+    cv::Mat_<double> R = read_matrix(mat_fol + "R.txt", 3, 3, 2);
+    cv::Mat_<double> t = read_matrix(mat_fol + "t.txt", 3, 1, 2);
+    cv::Mat_<double> kL = read_matrix(mat_fol + "kL.txt", 3, 3, 2);
+    cv::Mat_<double> kR = read_matrix(mat_fol + "kR.txt", 3, 3, 2);
+    cv::Mat_<double> distL = read_matrix(mat_fol + "distL.txt", 1, 4, 2);
+    cv::Mat_<double> distR = read_matrix(mat_fol + "distR.txt", 1, 4, 2);
+
+    Mat rectL, rectR;
+    rectify_pair(img1, img2, kL, kR, distL, distR, R, t, rectL, rectR);
+    cv::Mat res = half_dim(create_4_view(img1, img2, rectL, rectR));
+    cv::imshow("pr", res);
     cv::waitKey(0);
-    
-    
-    String matrices_folder = data_folder + "matrices/";
-    String Rpath = matrices_folder + "R.txt";
-    String tPath = matrices_folder + "t.txt";
-    String FPath = matrices_folder + "F.txt";
-    
-
-
-    cv::Mat_<double> R = read_matrix(Rpath,3,3,2);
-    write_matrix("R.txt", R, true);
-
-    
 
     return 0;
 }
